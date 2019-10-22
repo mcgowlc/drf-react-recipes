c856b323931a16811f16d83300481a96f7d447ca
Linda <mcgowlc@live.com>
2019-10-22 10:49:18 -0400

    connecting 3000 and 8000

---
+++ frontend/static/build/precache-manifest.16ec69b778bff5302b27adf724d78684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9b0c89cc60be4138f2d089c4c06792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6dfbb418aa0e893edc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c5f556b58ee8825dca6",
+    "url": "/static/js/2.0760e9c0.chunk.js"
+  },
+  {
+    "revision": "dc6dfbb418aa0e893edc",
+    "url": "/static/js/main.8f2e6f66.chunk.js"
+  },
+  {
+    "revision": "71dcedd2f2bbe7dcbd91",
+    "url": "/static/js/runtime-main.107b9db7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
