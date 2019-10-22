d24ba66dd40d470fb2c633e035df54274f9b4cc8
Linda <mcgowlc@live.com>
2019-10-22 12:06:09 -0400

    pushing changing

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div> Hello World</div>
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
+class App extends Component {
 
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      image: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleImageChange = this.handleImageChange.bind(this)
+  }
+
+  handleChange() {
+
+  }
+
+  handleImageChange() {
+
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" name='title' value= {this.state.title} onChange={this.handleChange} />
+        <input type="file" name='image'  onChange={this.handleImageChange} />
+        <button>Upload</button>
+      </form>
+    )
+  }
 }
 
 export default App;
